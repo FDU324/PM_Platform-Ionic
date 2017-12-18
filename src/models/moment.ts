d596a12a7083b7df03c36e198f96a61a4bbf002f
kadoufall <624938994@qq.com>
2017-12-18 22:00:15 +0800

    add moment model

---
+++ src/models/moment.ts
@@ -0,0 +1,10 @@
+export class Moment {
+    constructor(
+        public id: string,
+        public username: string,
+        public gameId: string,
+        public time: number,
+        public title: string,
+        public image: string, ) {
+    }
+}
